f1650b9b0e016b3f3c28a21a6389876dcde04082
Lorena Aylen <lag9512@gmail.com>
2024-05-25 16:22:33 -0300

    Fix: actualizar perfil
    Se deshabilitó endpoint actualizarImagen

--- Backend/routes/usuario.js
+++ Backend/routes/usuario.js
@@ -120,28 +120,6 @@ router.put('/modificarPassword', checkSchema(validacionesPass), (req, res) =>{
 
 })
 
-router.put('/actualizarImagen', (req, res)=>{
-  if(!req.body.imagen || !req.body.email){
-    res.status(400).json('Error. Imagen y mail obligatorios.')
-    return
-  }
-db.query(`UPDATE usuarios SET imagen = '${req.body.imagen}' WHERE email = '${req.body.email}'`, function(error, results){
-  if(error){
-    res.send({
-      success: false,
-      message: error
-    })
-    return
-  } else {
-    res.send({
-      success: true,
-      message: 'Imagen actualizada'
-    })
-  }
-})
-})
-
-
 router.delete('/eliminar', (req, res) => {
   const email = req.body.email
   console.log(email)
@@ -171,5 +149,30 @@ router.delete('/eliminar', (req, res) => {
   })
 
 })
+
+//#region Deshabilitados
+
+// router.put('/actualizarImagen', (req, res)=>{
+//   if(!req.body.imagen || !req.body.email){
+//     res.status(400).json('Error. Imagen y mail obligatorios.')
+//     return
+//   }
+// db.query(`UPDATE usuarios SET imagen = '${req.body.imagen}' WHERE email = '${req.body.email}'`, function(error, results){
+//   if(error){
+//     res.send({
+//       success: false,
+//       message: error
+//     })
+//     return
+//   } else {
+//     res.send({
+//       success: true,
+//       message: 'Imagen actualizada'
+//     })
+//   }
+// })
+// })
+
+//#endregion
 
 module.exports = router
